61eddc9bd7f914eba1732eb27d0a00730facc44a
Sunil Kumar C <sunil130524@gmail.com>
2024-11-15 09:38:10 +0530

    Implement Appointment list

--- src/app/appointment-list/appointment-list.component.ts
+++ src/app/appointment-list/appointment-list.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Appointment } from '../models/appointment';
 
 @Component({
   selector: 'app-appointment-list',
   templateUrl: './appointment-list.component.html',
   styleUrls: ['./appointment-list.component.css']
 })
-export class AppointmentListComponent {
-  appointment = 'Complete course'
+export class AppointmentListComponent implements OnInit{
+  appointmentTitle = ''
+  appointmentDate = new Date()
+  appointments: Appointment[] = []
+
+  ngOnInit(): void {
+    let savedAppointments = localStorage.getItem('appointments');
+    this.appointments = savedAppointments ? JSON.parse(savedAppointments) : []
+  }
+
+  addAppointment() {
+    if(this.appointmentTitle.trim().length && this.appointmentDate) {
+      this.appointments.push({
+        id: Date.now(),
+        title: this.appointmentTitle,
+        date: this.appointmentDate
+      })
+
+      localStorage.setItem('appointments', JSON.stringify(this.appointments));
+
+      this.appointmentTitle = ''
+      this.appointmentDate = new Date()
+    }
+  }
+
+  deleteAppointment(index: number) {
+    this.appointments.splice(index, 1);
+    localStorage.setItem('appointments', JSON.stringify(this.appointments));
+  }
 }
